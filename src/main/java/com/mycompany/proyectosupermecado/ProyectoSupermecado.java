7b7aa40b07bc0eabab0b29c78ad3254bbd31936a
GuillermoCamino <guillermocaminorodriguez@gmail.com>
2025-11-03 12:54:21 +0100

    Mi primer commit

---
+++ src/main/java/com/mycompany/proyectosupermecado/ProyectoSupermecado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectosupermecado;
+
+/**
+ *
+ * @author MEDAC
+ */
+public class ProyectoSupermecado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
