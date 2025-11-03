c79a20e2b64b9407f8bc8cead8abb7da4791dfc9
MEDAC <MEDAC@DESKTOP-S3BKR4Q>
2025-11-03 13:25:42 +0100

    Para dario, celosos

--- src/main/java/com/mycompany/proyectosupermecado/ProyectoSupermecado.java
+++ src/main/java/com/mycompany/proyectosupermecado/ProyectoSupermecado.java
@@ -12,5 +12,6 @@ public class ProyectoSupermecado {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Dario te amo");
     }
 }
