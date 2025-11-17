39df40dfd13bf93c746f3144eda009e14179283a
GuillermoCamino <92096380+GuillermoCamino@users.noreply.github.com>
2025-11-17 13:56:12 +0100

    Modificar clientes y dar de baja pasa paramentros de login a cliente

--- src/main/java/com/mycompany/proyectosupermecado/VentanaLogin.java
+++ src/main/java/com/mycompany/proyectosupermecado/VentanaLogin.java
@@ -146,8 +146,10 @@ public class VentanaLogin extends javax.swing.JFrame {
             //CASO DE SER EMPLEADO puedes poner su codigo de empleado y DNI
               if ((email.equals("bichoprime@gmail.com")||DNI.equals("12345A")||codigo.equals("720AR")||nombre_usuario.equals("CR7")) && contrasena.equals("1234")) {
              String nombre=TxtUsuario.getText().toString();
-            VentanaPrincipalEmpleado home_empleado = new VentanaPrincipalEmpleado(nombre);
-            home_empleado.setVisible(true);
+            String dni = TxtUsuario.getText(); // O el DNI real si lo tienes guardado
+            VentanaPrincipalEmpleado v = new VentanaPrincipalEmpleado(nombre, dni);
+            v.setVisible(true);
+            this.dispose();
             this.dispose();
         } else if (email.equals("Escribe aqui el nombre del usuario") || contrasena.equals("Aqui la contra")) {
             JOptionPane.showMessageDialog(
